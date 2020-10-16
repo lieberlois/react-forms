ad4af4ccda53cdbbe3707f0ff18951c9928b2bf1
Luis Schweigard <luis.schweigard@hs-augsburg.de>
2020-10-16 12:21:25 +0200

    Initial State

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+import { FormDemo } from './FormDemo';
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <FormDemo />
+    </Container>
+  );
+};
+
+export default App;
